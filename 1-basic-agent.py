6c0087a4b7408eb0e8bc25d24ac8592efedf98c6
Faizal Nursaori <faizalrahmatn@gmail.com>
2025-05-04 23:17:27 +0700

    basic-agent

---
+++ 1-basic-agent.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+from agents import Agent, Runner
+
+load_dotenv()
+
+agent = Agent(
+    name="Assistant",
+    instructions="You are a helpful assistant",
+    model="gpt-4.1-mini"
+)
+
+result = Runner.run_sync(agent, input="Write a job about programmer")
+print(result.final_output)
